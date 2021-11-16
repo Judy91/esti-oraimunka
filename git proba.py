67ad8401ce31b7d678d218239b1adcd2703131ad
Tanuló 301-10 <301-10@KKSZKI.LOCAL>
2021-11-16 18:28:42 +0100

    Elso feladat

--- git proba.py
+++ git proba.py
@@ -1,3 +1,5 @@
+import random
+
 print("git proba")
 print("Ez már a második commit")
 print ("Jó lesz ez")
@@ -7,3 +9,11 @@ szam2=int(input("Kérek 2 számot a felhasználótól"))
 osszeg=szam1+szam2
 print(osszeg)
 
+
+list=[]
+for i in range(10):
+    list.append(random.randrange(1,10))
+
+#Kiirom a listat
+for i in list:
+    print(i) 
