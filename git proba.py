61c15a2d1c4ffe66e3982f37188f7a58f77509eb
Tanuló 301-10 <301-10@KKSZKI.LOCAL>
2021-11-16 18:10:55 +0100

    Valami

--- git proba.py
+++ git proba.py
@@ -2,8 +2,8 @@ print("git proba")
 print("Ez már a második commit")
 print ("Jó lesz ez")
 
-szam=input("Kérek 1 számot a felhasználótól")
-szam2=input("Kérek 2 számot a felhasználótól")
-szam1=12
-szam2=28
-összeg=40
+szam1=int(input("Kérek 1 számot a felhasználótól"))
+szam2=int(input("Kérek 2 számot a felhasználótól"))
+osszeg=szam1+szam2
+print(osszeg)
+
