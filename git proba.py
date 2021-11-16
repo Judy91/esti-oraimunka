08f676eb0f37eeaeffe91a0ad92949f740442ac7
Tanuló 301-10 <301-10@KKSZKI.LOCAL>
2021-11-16 17:59:28 +0100

    Elso feladat

--- git proba.py
+++ git proba.py
@@ -1,3 +1,9 @@
 print("git proba")
 print("Ez már a második commit")
 print ("Jó lesz ez")
+
+szam=input("Kérek 1 számot a felhasználótól")
+szam2=input("Kérek 2 számot a felhasználótól")
+szam1=12
+szam2=28
+összeg=40
